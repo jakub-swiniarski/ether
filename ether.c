3fbfb42632a3baec616cb11be50fc7b630e777a9
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-02-11 13:49:29 +0100

    raw mode

--- ether.c
+++ ether.c
@@ -1,7 +1,29 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
+void disable_raw_mode(void);
+void enable_raw_mode(void);
+
+struct termios orig_termios;
+
+void disable_raw_mode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enable_raw_mode(void) {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disable_raw_mode);
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(void){
-    printf("TEST\n");
+    enable_raw_mode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 
     return 0;
 }
