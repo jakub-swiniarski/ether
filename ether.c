905c0f796529dbe6a5b3bf1ec3bd11f60d426e7f
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-02-12 18:35:23 +0100

    get cursor position

--- ether.c
+++ ether.c
@@ -23,6 +23,7 @@ static void die(const char *s);
 static void disable_raw_mode(void);
 static void draw_rows(void);
 static void enable_raw_mode(void);
+static int get_cursor_position(int *rows, int *cols);
 static int get_window_size(int *rows, int *cols);
 static void init(void);
 static void process_key(void);
@@ -70,10 +71,36 @@ void enable_raw_mode(void) {
         die("tcsetattr");
 }
 
+int get_cursor_position(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if (buf[i] == 'R')
+            break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return -1;
+}
+
 int get_window_size(int *rows, int *cols) {
     struct winsize ws;
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == 1 || ws.ws_col == 0)
-        return -1;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == 1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;
+        return get_cursor_position(rows, cols);
+    }
     else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
