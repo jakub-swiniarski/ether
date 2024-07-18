3b12f2f6546f008d56eaffe91a0f4ce7f10811a7
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-07-18 15:58:18 +0200

    delete char using x

--- config.h
+++ config.h
@@ -1,15 +1,17 @@
 /* keys */
 #define KEY_QUIT 'q'
+#define KEY_DELETE 'x'
 
 #define KEY_LEFT 'h'
 #define KEY_DOWN 'j'
 #define KEY_UP 'k'
 #define KEY_RIGHT 'l'
 
-/* modes */
-static const char *modes[] = { "NORMAL", "INSERT", "COMMAND" }; /* display names */
 #define KEY_INSERT 'i'
 #define KEY_COMMAND ':'
+
+/* modes */
+static const char *modes[] = { "NORMAL", "INSERT", "COMMAND" }; /* display names */
 
 /* other */
 #define TAB_STOP 4
